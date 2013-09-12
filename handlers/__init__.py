16e2d191a7bc0c2d3fa65a57f9988785aac04844
Simon Mathieu <simon.math@gmail.com>
2013-09-11 22:16:44 -0700

    Subscripbe api

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -10,6 +10,22 @@ config = getattr(__import__("config.%s" % env), env)
 SETTINGS = config.settings()
 client = github3.login(SETTINGS['github_user'], SETTINGS['github_pass'])
 
+class Issue(object):
+    def __init__(self, g3_issue):
+        self.g3_issue = g3_issue
+
+    def to_json(self):
+        return  {
+            'id': self.g3_issue.id,
+            'body': self.g3_issue.body_text,
+            'num_subscribers': 5
+        }
+
+    def add_subscriber(self, email):
+        b = self.g3_issue.body_text = "\nlisteners: " + email
+        self.g3_issue.edit(body = b)
+
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.render(
@@ -24,10 +40,9 @@ class IssuesHandler(tornado.web.RequestHandler):
         r = client.repository('rainforestapp', 'GitSatisfaction')
         out = []
         for issue in r.iter_issues(state='open', labels='gs'):
-            out.append({'id': issue.id,
-                'body': issue.body_text,
-                'num_subscribers': 5})
+            out.append(Issue(issue).to_json())
         self.write(json.dumps(out))
+
     def post(self):
         r = client.repository('rainforestapp', 'GitSatisfaction')
         print self.request.body
@@ -35,6 +50,17 @@ class IssuesHandler(tornado.web.RequestHandler):
         label = r.label('gs')
         if not label: label = r.create_label('gs', '#00ffff')
         r.create_issue(new_issue['title'], body=new_issue['body'], labels='[gs]')
+
+class SubscribeHandler(tornado.web.RequestHandler):
+    def post(self, issue_id):
+        r = client.repository('rainforestapp', 'GitSatisfaction')
+        issue = Issue(r.issue(issue_id))
+
+        j = tornado.escape.json_decode(self.request.body)
+
+        issue.add_subscriber(j["email"])
+
+        self.write(issue.to_json())
 
 class GithubCallbackHandler(tornado.web.RequestHandler):
     def post(self, q):
--- tests.py
+++ tests.py
@@ -1,5 +1,6 @@
 from tornado import testing
 from os import environ
+import json
 
 environ['GITHUB_USER'] = 'gitsatisfaction'
 environ['GITHUB_PASS'] = 'Password123'
@@ -14,4 +15,13 @@ class IssuesTest(testing.AsyncHTTPTestCase):
     def test_issues(self):
         self.http_client.fetch(self.get_url('/issues/'), self.stop)
         response = self.wait()
-        self.assertIn("hello", response.body)
+        self.assertIn("[]", response.body)
+
+    def test_subscribe(self):
+        self.http_client.fetch(self.get_url('/issues/1/subscribe'), self.stop,
+                method="POST", body='{"email": "foo@bar.com"}')
+        response = self.wait()
+        j = json.loads(response.body)
+        self.assertTrue(j)
+        self.assertIn("foo@bar", j["body"])
+
