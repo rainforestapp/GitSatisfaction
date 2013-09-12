0eafe66b0a6d55abac804fcaa749c752ebc54c60
Simon Mathieu <simon.math@gmail.com>
2013-09-11 18:40:34 -0700

    Remove the comments

--- server.py
+++ server.py
@@ -1,12 +1,3 @@
-#    Sample main.py Tornado file
-#    (for Tornado on Heroku)
-#
-#    Author: Mike Dory | dory.me
-#    Created: 11.12.11 | Updated: 06.02.13
-#    Contributions by Tedb0t, gregory80
-#
-# ------------------------------------------
-
 #!/usr/bin/env python
 import os.path
 import tornado.escape
@@ -15,12 +6,10 @@ import tornado.ioloop
 import tornado.options
 import tornado.web
 
-# import and define tornado-y things
 from tornado.options import define
 define("port", default=5000, help="run on the given port", type=int)
 
 
-# application settings and handle mapping info
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
@@ -34,7 +23,6 @@ class Application(tornado.web.Application):
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
-# the main page
 class MainHandler(tornado.web.RequestHandler):
     def get(self, q):
         if 'GOOGLEANALYTICSID' in os.environ:
@@ -49,13 +37,11 @@ class MainHandler(tornado.web.RequestHandler):
         )
 
 
-# RAMMING SPEEEEEEED!
 def main():
     tornado.options.parse_command_line()
     http_server = tornado.httpserver.HTTPServer(Application())
     http_server.listen(tornado.options.options.port)
 
-    # start it up
     tornado.ioloop.IOLoop.instance().start()
 
 
