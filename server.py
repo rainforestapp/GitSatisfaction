1e237b67c9e0cb73dd781c95e423082029b9be41
Simon Mathieu <simon.math@gmail.com>
2013-09-11 18:16:16 -0700

    Server and assets

--- server.py
+++ server.py
@@ -1,14 +1,65 @@
+#    Sample main.py Tornado file
+#    (for Tornado on Heroku)
+#
+#    Author: Mike Dory | dory.me
+#    Created: 11.12.11 | Updated: 06.02.13
+#    Contributions by Tedb0t, gregory80
+#
+# ------------------------------------------
+
+#!/usr/bin/env python
+import os.path
+import tornado.escape
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+# import and define tornado-y things
+from tornado.options import define
+define("port", default=5000, help="run on the given port", type=int)
 
-application = tornado.web.Application([
-    (r"/", MainHandler),
-])
+
+# application settings and handle mapping info
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/([^/]+)?", MainHandler)
+        ]
+        settings = dict(
+            template_path=os.path.join(os.path.dirname(__file__), "templates"),
+            static_path=os.path.join(os.path.dirname(__file__), "static"),
+            debug=True,
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+
+# the main page
+class MainHandler(tornado.web.RequestHandler):
+    def get(self, q):
+        if 'GOOGLEANALYTICSID' in os.environ:
+            google_analytics_id = os.environ['GOOGLEANALYTICSID']
+        else:
+            google_analytics_id = False
+
+        self.render(
+            "main.html",
+            page_title='Heroku Funtimes',
+            page_heading='Hi!',
+            google_analytics_id=google_analytics_id,
+        )
+
+
+# RAMMING SPEEEEEEED!
+def main():
+    tornado.options.parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(tornado.options.options.port)
+
+    # start it up
+    tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
-    application.listen(8888)
-    tornado.ioloop.IOLoop.instance().start()
+    main()
+
